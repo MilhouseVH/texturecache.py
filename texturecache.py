f278963fd83a50ab949b5ccbf5671348437b7e69
MilhouseVH <milhouseVH.github@nmacleod.com>
2013-04-11 03:36:07 +0100

    Add vscan, ascan, vclean etc. to checkConfiguration()

--- texturecache.py
+++ texturecache.py
@@ -2287,12 +2287,13 @@ def checkConfig(option):
   else:
     needWeb = False
 
-  if option in ["c","C","nc","lc", "lnc", "j","jd","J","Jd","qa","qax","p","P" ]:
+  if option in ["c","C","nc","lc","lnc","j","jd","J","Jd","qa","qax","p","P",
+                "vscan", "ascan", "vclean", "aclean", "directory", "sources"]:
     needSocket = True
   else:
     needSocket = False
 
-  if option in ["s","S","x","X","f","c","C","nc","lc", "lnc", "qa","qax","d","r","R","p","P" ]:
+  if option in ["s","S","x","X","f","c","C","nc","lc","lnc","qa","qax","d","r","R","p","P" ]:
     needDb = True
   else:
     needDb = False
