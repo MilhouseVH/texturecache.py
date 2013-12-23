19dc35994bfc68bc5f57aed538b29f198983c201
MilhouseVH <milhouseVH.github@nmacleod.com>
2013-12-23 16:43:55 +0000

    Add argparse support to tools\clean.py

--- tools/clean.py
+++ tools/clean.py
@@ -26,47 +26,65 @@
 #
 # Usage:
 #
-#  ./texturecache.py jd movies | ./clean.py pattern [pattern]*
+#  ./texturecache.py jd movies | ./clean.py --patterns [pattern]* --keep [type]*
 #
 # eg.
 #
-#  ./texturecache.py jd movies  | ./clean.py /movielogo/-51fd12c146bd9\.png
-#  ./texturecache.py jd movies  | ./clean.py /moviethumb/ /moviedisc/ /moviebanner/
-#  ./texturecache.py jd tvshows | ./clean.py assets\.fanart\.tv
+#  ./texturecache.py jd movies  | ./clean.py --patterns /movielogo/-51fd12c146bd9\.png
+#  ./texturecache.py jd movies  | ./clean.py --patterns /moviethumb/ /moviedisc/ /moviebanner/
+#  ./texturecache.py jd tvshows | ./clean.py --patterns assets\.fanart\.tv
+#  ./texturecache.py jd tvshows | ./clean.py --keep fanart poster clearart clearlogo
+#
+#  Keep specific artwork types, but "clean" any other types that are remote:
+#
+#  ./texturecache.py jd tvshows | ./clean.py --keep fanart poster clearart clearlogo --patterns http://
 #
 ################################################################################
 
-#version 0.1.0
+#version 0.2.0
 
 from __future__ import print_function
-import sys, os, codecs, json, re
+import sys, os, codecs, json, re, argparse
 
-def init():
-  if sys.version_info >= (3, 1):
-    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
-    sys.stderr = codecs.getwriter("utf-8")(sys.stderr.detach())
-  else:
-    sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
-    sys.stderr = codecs.getwriter("utf-8")(sys.stderr)
+def printout(msg, newline=True):
+  endchar = "\n" if newline else ""
+  print(msg, file=sys.stdout, end=endchar)
+  sys.stderr.flush()
 
-def getdata():
-  data=[]
-  for line in sys.stdin: data.append(line)
-  jdata = json.loads("".join(data))
-  del data
-  return jdata
+def printerr(msg, newline=True):
+  endchar = "\n" if newline else ""
+  print(msg, file=sys.stderr, end=endchar)
+  sys.stderr.flush()
 
-def processitems(data, patterns):
+def addEllipsis(maxlen, aStr):
+  if len(aStr) <= maxlen: return aStr
+
+  ileft = int(maxlen/2) - 2
+  iright = int(maxlen/2) - 1
+
+  return "%s...%s" % (aStr[0:ileft], aStr[-iright:])
+
+def debug(msg):
+  global VERBOSE
+  if VERBOSE: printerr(msg)
+
+def processitems(data, keepart, patterns):
   workitems=[]
 
   for item in data:
     items = {}
     for a in item.get("art", []):
-      if not a.startswith("tvshow."):
-        url = re.sub("^image://(.*)/", "\\1", item["art"][a])
-        for pattern in patterns:
-          if pattern.search(url):
-            items["art.%s" % a] = None
+      if not a.startswith("tvshow.") and a not in keepart:
+        if not patterns:
+          debug("Not Keep: [%-12s] %-50s [%s]" % (a, addEllipsis(50, item.get("title", item.get("label", None))), item["art"][a]))
+          items["art.%s" % a] = None
+        else:
+          url = re.sub("^image://(.*)/", "\\1", item["art"][a])
+          for pattern in patterns:
+            if pattern.search(url):
+              debug("Pattern:  [%-12s] %-50s [%s]" % (a, addEllipsis(50, item.get("title", item.get("label", None))), item["art"][a]))
+              items["art.%s" % a] = None
+              break
 
     if items:
       if "movieid" in item:
@@ -78,31 +96,70 @@ def processitems(data, patterns):
       elif "episodeid" in item:
         workitems.append({"items": items, "libraryid": item["episodeid"], "type": "episode", "title": item["label"]})
 
-    if "seasons" in item:  workitems.extend(processitems(item["seasons"], patterns))
-    if "episodes" in item: workitems.extend(processitems(item["episodes"], patterns))
+    if "seasons" in item:  workitems.extend(processitems(item["seasons"], keepart, patterns))
+    if "episodes" in item: workitems.extend(processitems(item["episodes"], keepart, patterns))
 
   return workitems
 
-def main(args):
-  init()
+def getdata():
+  data=[]
+  for line in sys.stdin: data.append(line)
+  jdata = json.loads("".join(data))
+  del data
+  return jdata
 
+def init():
+  global VERBOSE
+
+  if sys.version_info >= (3, 1):
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+    sys.stderr = codecs.getwriter("utf-8")(sys.stderr.detach())
+  else:
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+    sys.stderr = codecs.getwriter("utf-8")(sys.stderr)
+
+  parser = argparse.ArgumentParser(description="Generate instructions to remove artwork urls matching specific patterns \
+                                                or that are of a type that should not be retained", \
+                    formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=25,width=90))
+
+  parser.add_argument("-k", "--keepart", nargs="+", metavar="TYPE", \
+                      help="Artwork TYPE(s) to be kept or retained, eg. \"--keepart fanart poster\"")
+
+  parser.add_argument("-p", "--patterns", nargs="+", metavar="PATTERN", \
+                      help="Regex pattern(s) to match against artwork urls (unless artwork type is \
+                            specified in --keepart)")
+
+  parser.add_argument("-v", "--verbose", action="store_true", \
+                      help="Display diagnostic output")
+
+  args = parser.parse_args()
+
+  VERBOSE = args.verbose
+
+  if args.keepart == None and args.patterns == None:
+    parser.error("At least --keepart or --patterns must be specified")
+
+  return(args)
+
+def main(args):
+
+  keepart = []
   patterns = []
 
-  # If no args, default to a logo pattern that is erroneously
-  # added by Artwork Downloader
-  if not args:
-    args.append("/movielogo/-51fd12c146bd9\.png")
+  if args.keepart:
+    keepart = args.keepart
 
-  for arg in args:
-    patterns.append(re.compile(arg))
+  if args.patterns:
+    for arg in args.patterns:
+      patterns.append(re.compile(arg))
 
-  workitems = processitems(getdata(), patterns)
+  workitems = processitems(getdata(), keepart, patterns)
 
   sys.stdout.write(json.dumps(workitems, indent=2))
   sys.stdout.write("\n")
   sys.stdout.flush()
 
 try:
-  main(sys.argv[1:])
+  main(init())
 except (KeyboardInterrupt, SystemExit) as e:
   if type(e) == SystemExit: sys.exit(int(str(e)))
