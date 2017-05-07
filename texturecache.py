MilhouseVH/texturecache.py

commit d6ce1b21d55412a8a498bd4a467e3f63e8bf850d

v2.3.6: Handle seasons without episodes, fixes #44

VERSION
@@ -1,1 +1,1 @@
-2.3.5 9ea5db58db6624364bb1ffe23d0f551a
+2.3.6 15331c649adc36625eadd8adcfef2a54

texturecache.py
@@ -60,7 +60,7 @@ lock = threading.RLock()
 class MyConfiguration(object):
   def __init__(self, argv):
 
-    self.VERSION = "2.3.5"
+    self.VERSION = "2.3.6"
 
     self.GITHUB = "https://raw.github.com/MilhouseVH/texturecache.py/master"
     self.ANALYTICS_GOOD = "http://goo.gl/BjH6Lj"
@@ -6822,6 +6822,8 @@ def getAllFiles(keyFunction):
                                "properties":["cast", "art", "file"]}}
 
           episodedata = jcomms.getDataProxy("episodes", REQUEST, uniquecast=UCAST)
+          if "episode" not in episodedata["result"]:
+            continue # ignore seasons without episodes
 
           for episode in episodedata["result"]["episodes"]:
             episodeid = episode["episodeid"]

commit ec16039cb590eb75c988eba4871db3cc8ddfa30b

v2.3.9: Fix bug introduced in prune by d6ce1b2

VERSION
@@ -1,1 +1,1 @@
-2.3.8 39335cc71c3b08ddb2e98401a80e4167
+2.3.9 cfbe4f76a3867a4115461ce4446a886d

texturecache.py
@@ -60,7 +60,7 @@ lock = threading.RLock()
 class MyConfiguration(object):
   def __init__(self, argv):
 
-    self.VERSION = "2.3.8"
+    self.VERSION = "2.3.9"
 
     self.GITHUB = "https://raw.github.com/MilhouseVH/texturecache.py/master"
     self.ANALYTICS_GOOD = "http://goo.gl/BjH6Lj"
@@ -6840,7 +6840,7 @@ def getAllFiles(keyFunction):
                                "properties":["cast", "art", "file"]}}
 
           episodedata = jcomms.getDataProxy("episodes", REQUEST, uniquecast=UCAST)
-          if "episode" not in episodedata["result"]:
+          if "episodes" not in episodedata["result"]:
             continue # ignore seasons without episodes
 
           for episode in episodedata["result"]["episodes"]: